6fef2aaa3121bc2f06c15365086663185b57c5fe
welcome <anandhi@systimanx.com>
2019-06-24 23:13:23 +0530

    chnages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './public/layouts/header/header.component';
+import { FooterComponent } from './public/layouts/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent
   ],
+  exports: [
+   HeaderComponent,
+   FooterComponent,
+   HomeComponent,
+   AboutComponent
+	],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
